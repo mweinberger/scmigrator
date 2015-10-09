06ddf4f4de8bcfe3ac1898f84ae166bd8baa0d0f
Michael Weinberger <mweinberger@tenable.com>
2015-10-09 13:49:03 -0400

    changes to report calls

--- scmigrator.py
+++ scmigrator.py
@@ -44,7 +44,8 @@ def sc4_menus(sc4):
                            "  13. Import Dashboards \n"
                            "------Special------ \n"
                            "  14. Export EVERYTHING \n"
-                           "  15. Import EVERYTHING \n")
+                           "  15. Import EVERYTHING \n"
+                           ":")
         if switch == "1":
             return
         elif switch == "2":
@@ -87,25 +88,26 @@ def sc5_menus(sc5):
 
     while True:
         switch = raw_input("\nAvailable SC5 Menu Options: \n1. Back to Main Menu \n"
-                           "------Assets------"
+                           "------Assets------ \n"
                            "  2. List Assets \n"
                            "  3. Export Assets \n"
                            "  4. Import Assets \n"
-                           "------Policies------"
+                           "------Policies------ \n"
                            "  5. List Policies \n"
                            "  6. Export Policies \n"
                            "  7. Import Policies \n"
-                           "------Reports------"
+                           "------Reports------ \n"
                            "  8. List Reports \n"
                            "  9. Export Reports \n"
                            "  10. Import Reports \n"
-                           "------Dashboards------"
+                           "------Dashboards------ \n"
                            "  11. List Dashboards \n"
                            "  12. Export Dashboards \n"
                            "  13. Import Dashboards \n"
                            "------Special------ \n"
                            "  14. Export EVERYTHING \n"
-                           "  15. Import EVERYTHING \n")
+                           "  15. Import EVERYTHING \n"
+                           ":")
         if switch == "1":
             return
         elif switch == "2":
@@ -121,11 +123,11 @@ def sc5_menus(sc5):
         elif switch == "7":
             items.import_sc5(sc5, 'policy')
         elif switch == "8":
-            items.get_sc5(sc5, 'report')
+            items.get_sc5(sc5, 'reportDefinition')
         elif switch == "9":
-            items.export_sc5(sc5, 'report')
+            items.export_sc5(sc5, 'reportDefinition')
         elif switch == "10":
-            items.import_sc5(sc5, 'report')
+            items.import_sc5(sc5, 'reportDefinition')
         elif switch == "11":
             items.get_sc5(sc5, 'dashboard')
         elif switch == "12":
@@ -135,12 +137,12 @@ def sc5_menus(sc5):
         elif switch == "14":
             items.export_sc5(sc5, 'asset', all=True)
             items.export_sc5(sc5, 'policy', all=True)
-            items.export_sc5(sc5, 'report', all=True)
+            items.export_sc5(sc5, 'reportDefinition', all=True)
             items.export_sc5(sc5, 'dashboard', all=True)
         elif switch == "15":
             items.import_sc5(sc5, 'asset', all=True)
             items.import_sc5(sc5, 'policy', all=True)
-            items.import_sc5(sc5, 'report', all=True)
+            items.import_sc5(sc5, 'reportDefinition', all=True)
             items.import_sc5(sc5, 'dashboard', all=True)
 
 
@@ -225,5 +227,5 @@ while True:
             print "Exiting..."
             break
 
-connect.logout()
+connect.logout(sc4, sc5)
 exit()
